6556f8e8114bdd98d596779f0e2367d88df51d04
usamasadiq029 <usamasadiq029@gmail.com>
2023-06-26 04:39:48 +0500

    apply condition with alert message

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,21 +6,25 @@ const resultElement = document.getElementById("result"); // Result element
 
 // Function to guess if the result is an even or odd number
 function guessEvenOddNumber() {
-  // Get the values from the input fields and convert them to numbers
-  const value1 = Number(input1.value);
-  const value2 = Number(input2.value);
+  if (Number(input1.value) !== 0 && Number(input2.value) !== 0) {
+    // Get the values from the input fields and convert them to numbers
+    const value1 = Number(input1.value);
+    const value2 = Number(input2.value);
 
-  // Perform the modulo operation to check if value1 is divisible by value2
-  let output;
+    // Perform the modulo operation to check if value1 is divisible by value2
+    let output;
 
-  if (value1 % value2 === 0) {
-    output = "The result is an even number.";
-  } else {
-    output = "The result is an odd number.";
+    if (value1 % value2 === 0) {
+      output = "The result is an even number.";
+    } else {
+      output = "The result is an odd number.";
+    }
+
+    // Update the result element with the output message
+    resultElement.textContent = output;
   }
 
-  // Update the result element with the output message
-  resultElement.textContent = output;
+  alert("Please enter values in both input fields.");
 }
 
 // Add event listener to the add button
